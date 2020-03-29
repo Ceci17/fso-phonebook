fb7e62e4aa6472b55be09f444f672f33b9ea335d
Stefan Nikolic <stefan.nikolic@united.cloud>
2020-03-29 13:30:37 +0200

    add fe build

---
+++ build/precache-manifest.e838019e7a745f4e36588ded3a8ecfa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9c7fac1b7a614fb714327f0e02125e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "661897aa6bf1f5487e07",
+    "url": "/static/css/main.907ea433.chunk.css"
+  },
+  {
+    "revision": "fcc6fb31e2e16c5e3ecf",
+    "url": "/static/js/2.55ca0358.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.55ca0358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661897aa6bf1f5487e07",
+    "url": "/static/js/main.773838a1.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
